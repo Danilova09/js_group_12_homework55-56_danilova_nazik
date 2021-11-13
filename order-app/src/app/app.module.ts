c0347b36a169dd98b1829739ef93e57b70622f6f
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-11-13 09:03:17 +0600

    Реализация компонента ingredients-block,стилизация, добавление основных функций

--- order-app/src/app/app.module.ts
+++ order-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IngredientsBlockComponent } from './ingredients-block/ingredients-block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngredientsBlockComponent
   ],
   imports: [
     BrowserModule
---
+++ order-app/src/app/ingredients-block/ingredients-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredients-block',
+  templateUrl: './ingredients-block.component.html',
+  styleUrls: ['./ingredients-block.component.css']
+})
+export class IngredientsBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
